7ac23506774f467940363fe6435a16735079815f
Amen Wondwosen <amenwond408@gmail.com>
2022-04-30 13:26:19 -0700

    Updated sql into insert values into schema

---
+++ db/10_matching.sql
@@ -0,0 +1,2 @@
+INSERT INTO projects.matching (course_instance_id, project_id, team_id)
+VALUES  (36050, 1, 1);
---
+++ db/4_team.sql
@@ -0,0 +1,13 @@
+INSERT INTO projects.team (id, name, size)
+VALUES  (1, '191 for 191 ', 5),
+        (2, 'AP-Team', 5),
+        (3, 'Blue Box', 5),
+        (4, 'Coast', 5),
+        (5, 'Flick Power', 6),
+        (6, 'Genres', 5),
+        (7, 'HOLO', 5),
+        (8, 'Learn Collaborate', 5),
+        (9, 'Monocle', 5),
+        (10, 'Nutripair', 5),
+        (11, 'Smartpreneur', 6),
+        (12, 'ZUUM', 6);
