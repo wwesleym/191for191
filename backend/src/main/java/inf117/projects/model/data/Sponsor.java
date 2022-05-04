2b1c28a0224997f08d59646b880bb543ad0322e8
Amen Wondwosen <amenwond408@gmail.com>
2022-05-03 22:51:51 -0700

    created functions to insert projects and get/insert sponsors

---
+++ backend/src/main/java/inf117/projects/model/data/Sponsor.java
@@ -0,0 +1,34 @@
+package inf117.projects.model.data;
+
+public class Sponsor {
+    private int id;
+    private String name;
+    private String website;
+
+    public int getId() {
+        return id;
+    }
+
+    public Sponsor setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Sponsor setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public Sponsor setWebsite(String website) {
+        this.website = website;
+        return this;
+    }
+}
--- backend/src/main/java/inf117/projects/repo/ProjectRepo.java
+++ backend/src/main/java/inf117/projects/repo/ProjectRepo.java
@@ -2,6 +2,7 @@ package inf117.projects.repo;
 
 import inf117.projects.model.data.Project;
 import inf117.projects.model.data.ProjectState;
+import inf117.projects.model.data.Sponsor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -9,6 +10,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.Types;
+import java.util.List;
 
 @Component
 public class ProjectRepo {
@@ -18,6 +20,10 @@ public class ProjectRepo {
     public ProjectRepo(NamedParameterJdbcTemplate template) {
         this.template = template;
     }
+
+    //language=sql
+    private static String PROJECT_WITH_DEFAULT =
+            "";
 
     public Project projectByProjectName(String name) {
         return this.template.queryForObject(
@@ -41,5 +47,70 @@ public class ProjectRepo {
     }
 
     public void insertProject(Project project) {
+        // Check if sponsor name in database
+        Sponsor sponsor;
+        if (project.getSponsorName() != null) {
+            sponsor = selectSponsor(project.getSponsorName());
+        } else {
+            sponsor = null;
+        }
+
+        // Insert new sponsor if sponsor name not in database
+        if (sponsor == null) {
+            sponsor = new Sponsor()
+                    .setId(project.getId())
+                    .setName(project.getSponsorName());
+
+            // Insert sponsor into database
+            this.insertSponsor(sponsor);
+        }
+
+        // Insert project in database
+        this.template.update(
+                "INSERT INTO projects.project (id, name, team_size, sponsor_id, " +
+                        "project_description, pitch_video, image, state, course_instance_id) " +
+                    "VALUES (:id, :name, :teamSize, :sponsorId, " +
+                        ":projectDescription, :pitchVideo, :image, :state, :courseId);",
+                new MapSqlParameterSource()
+                        .addValue("id", project.getId(), Types.INTEGER)
+                        .addValue("name", project.getName(), Types.VARCHAR)
+                        .addValue("teamSize", project.getTeamSize(), Types.INTEGER)
+                        .addValue("sponsorId", sponsor.getId(), Types.INTEGER)
+                        .addValue("projectDescription", project.getDescription(), Types.VARCHAR)
+                        .addValue("pitchVideo", project.getVideo(), Types.VARCHAR)
+                        .addValue("image", project.getImage(), Types.VARCHAR)
+                        .addValue("state", project.getState(), Types.VARCHAR)
+                        .addValue("courseId", project.getCourseId(), Types.INTEGER)
+        );
+
+    }
+
+    public Sponsor selectSponsor(String name) {
+        try {
+            return this.template.queryForObject(
+                    "SELECT s.id, s.name " +
+                            "FROM projects.sponsor s " +
+                            "WHERE s.name LIKE :name ",
+                    new MapSqlParameterSource()
+                            .addValue("name", '%'+name+'%', Types.VARCHAR),
+                    (rs, rowNum) ->
+                            new Sponsor()
+                                    .setId(rs.getInt("s.id"))
+                                    .setName(rs.getString("s.name"))
+                                    .setWebsite(rs.getString("s.website"))
+            );
+        } catch (DataAccessException e) {
+            return null;
+        }
+    }
+
+    public void insertSponsor(Sponsor sponsor) {
+        this.template.update(
+                "INSERT INTO projects.sponsor (id, name) " +
+                    "VALUES (:id, :name);",
+                new MapSqlParameterSource()
+                        .addValue("id", sponsor.getId(), Types.INTEGER)
+                        .addValue("name", sponsor.getName(), Types.VARCHAR)
+        );
     }
 }
