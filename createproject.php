b6e3b888c8fe0a5d78a78f763d1757ddf1023a2d
IsAndrewKim <103150232+IsAndrewKim@users.noreply.github.com>
2022-05-12 01:33:24 -0700

    Edited createproject.php to send data
    It's now possible to send data through a server. Although it is definitely a work in progress but it can now put data onto the server. Need more test things though.

--- createproject.php
+++ createproject.php
@@ -1,22 +1,65 @@
 <?php
-    $title =  $_POST['title']; //2, 
-    $ProjectDesc = $_POST['ProjectDesc'];//5
-    $CourseName = $_POST['CourseName']; // 9
-    $QuartYear = $_POST['QuartYear']; //10, 11
-    $Linkss = $_POST['Linkss']; //6
-    $SponsorName = $_POST['SponsorName'];//4
-    $TeamMem = $_POST['TeamMem'];
-    $ProjTags= $_POST['ProjTags']
+
+//id = random number for id that checks
+//name = title
+//team_size = number of student objects in the group start at 0
+//sponsor_id = basically the sponsor object NULL for now random number for id that checks
+//project_description = project description
+//pitch_video = links
+//image = links? NULL for now
+//state = 1 = notstart, 2 = inprogress, 3 = complete 
+//course_instance_id = the course code like 31483
+//year = year number
+//term = 1 = fall, 2 = winter, 3 = spring, 4 = summer
+
+
+    $fillerId = random_int(0,999);
+    $title =  $_POST['title'];  
+    $fillerTeamSize = 0;
+    $fillerSponsorId = 1;    
+    $ProjectDesc = $_POST['ProjectDesc'];
+    $Linkss = $_POST['Linkss']; 
+    $fillerImage = NULL;
+    $fillerState = 1; // will always be 1 because creation has happened unless team size is increased but so far we can't do that
+    $courseCode = NULL; //filler
+    //$courseCode = $_POST['courseCode'];
+    $yearInput = $_POST['yearInput']; // 9
+    $quarterInput = $_POST['quarterInput']; //also term
 
 
 
-    $conn = new mysqli('db.matthewbietz.org','191for191admin','Capstone22','191for191');
+    //$conn = new mysqli('db.matthewbietz.org','191for191admin','Capstone22','191for191');
+    //add some random number generator to make the id and check that id if it already exists.
+    //sponsor ids should be whateber
+
+    $conn = new mysqli('localhost','root','','191for191');
     if($conn->connect_error){
         echo "$conn->connect_error";
         die("Connection Failed : ". $conn->connect_error);
     } else {
-        $stmt = $conn->prepare("insert into project(id, name, project_description ) values(?, ?, ?");
-        $stmt->bind_param("iss", 4, $title, $ProjectDesc);
+
+
+        if ($quarterInput = 1) {
+            $quarterInput = "FALL";
+          } elseif ($quarterInput = 2) {
+            $quarterInput = "WINTER";
+          } elseif ($quarterInput = 3) {
+            $quarterInput = "SPRING";
+          } else {
+            $quarterInput = "SUMMER";
+          }
+
+        if ($fillerState = 1) {
+            $fillerState = "NOT_STARTED";
+          } elseif ($fillerState = 2) {
+            $fillerState = "IN_PROGRESS";
+          } else {
+            $fillerState = "COMPLETED";
+          } 
+
+
+        $stmt = $conn->prepare("insert into project(id, name, team_size, sponsor_id, project_description, pitch_video, image,state, course_instance_id, year, term) values(?,?,?,?,?,?,?,?,?,?,?)");
+        $stmt->bind_param("isiissssiis", $fillerId, $title,  $fillerTeamSize, $fillerSponsorId, $ProjectDesc,  $Linkss, $fillerImage, $fillerState, $courseCode,  $yearInput, $quarterInput);
         $execval = $stmt->execute();
         echo $execval;
         echo "Registration successfully...";
@@ -24,29 +67,5 @@
         $conn->close();
     }
 
-    /* https://www.9lessons.info/2008/12/twitter-used-jquery-plug-in.html 
-        idk what im doing here lol
-    */
-    
-    /*
-    if(isSet($_POST['username']))
-    {
-    $username = $_POST['username'];
-
-    include("dbconnection.php");
-    
-
-    $sql_check = mysql_query("SELECT course,quarter FROM backend/db/4_course_instance.sql WHERE course='$CourseName' AND quarter = '$QuartYear'");
-
-    if(mysql_num_rows($sql_check))
-    {
-    echo '<span style="color: red;">The course <b>'.$CourseName.'</b> does not exist.</span>';
-    }
-    else
-    {
-    echo 'OK';
-    }}  
-
-    */
 
 ?>
