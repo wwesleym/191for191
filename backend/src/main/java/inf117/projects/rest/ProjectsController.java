b5d5bd68c60f4fdbd604850641003d81e71a7fcb
ztran-slateGray <94199402+ztran-slateGray@users.noreply.github.com>
2022-05-19 09:33:13 +0700

    Add files via upload

--- backend/src/main/java/inf117/projects/rest/ProjectsController.java
+++ backend/src/main/java/inf117/projects/rest/ProjectsController.java
@@ -1,6 +1,6 @@
 package inf117.projects.rest;
 
-import com.codingsprojects.stack.core.result.Result;
+import inf117.projects.core.result.ProjectsResults;
 import inf117.projects.model.response.SponsorSearchResponseModel;
 import inf117.projects.repo.entity.Project;
 import inf117.projects.model.response.ProjectNameSearchResponseModel;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import inf117.projects.util.validate;
 
 @RestController
 public class ProjectsController {
@@ -21,50 +22,50 @@ public class ProjectsController {
         this.repo = repo;
     }
 
-    @PostMapping("/sample/search")
+    @GetMapping("/projects/search/name/")
     ProjectNameSearchResponseModel ProjectSearchByName(
             @RequestParam String name)
     {
         Project project = this.repo.projectByProjectName(name);
         ProjectNameSearchResponseModel response = new ProjectNameSearchResponseModel();
         response.setProject(project);
-        response.setResult(Result.NO_RESULT);
+        response.setResult(ProjectsResults.PROJECTS_FOUND_WITHIN_SEARCH);
 
         return response;
     }
 
-    @PostMapping("/sample/projectById")
+    @GetMapping("/projects/search/id/")
     ProjectNameSearchResponseModel ProjectSearchById(
             @RequestParam Integer Id)
     {
         Project project = this.repo.selectProjectId(Id);
         ProjectNameSearchResponseModel response = new ProjectNameSearchResponseModel();
         response.setProject(project);
-        response.setResult(Result.NO_RESULT);
+        response.setResult(ProjectsResults.PROJECT_WITH_ID_FOUND);
 
         return response;
     }
 
-    @PostMapping("/sample/sponsor")
+    @GetMapping("/sponsor/search/name/")
     SponsorSearchResponseModel SponsorSearchByName(
             @RequestParam String name)
     {
         Sponsor sponsor = this.repo.selectSponsor(name);
         SponsorSearchResponseModel response = new SponsorSearchResponseModel();
         response.setSponsor(sponsor);
-        response.setResult(Result.NO_RESULT);
+        response.setResult(ProjectsResults.SPONSORS_FOUND_WITHIN_SEARCH);
 
         return response;
     }
 
-    @PostMapping("/sample/sponsorId")
+    @GetMapping("/sponsor/search/id/")
     SponsorSearchResponseModel SponsorSearchById(
             @RequestParam Integer Id)
     {
         Sponsor sponsor = this.repo.selectSponsorId(Id);
         SponsorSearchResponseModel response = new SponsorSearchResponseModel();
         response.setSponsor(sponsor);
-        response.setResult(Result.NO_RESULT);
+        response.setResult(ProjectsResults.SPONSOR_WITH_ID_FOUND);
 
         return response;
     }
