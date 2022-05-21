26c9d915ad38a889fc7401506b2f992f208e7910
Amen Wondwosen <amenwond408@gmail.com>
2022-05-20 21:41:55 -0700

    fixed backend integration issue
    Removed reliance on sample project data. The response will still print to the console in case any new changes were made.

--- js/backend-talker.js
+++ js/backend-talker.js
@@ -1,13 +1,13 @@
 const options = {
-    method: "GET",
+    method: "POST",
     baseURL: "http://localhost:8082",
-    url: "/projects/search/id",  // for endpoints
+    url: "/projects/search/name",
     data: {
-        "id": 1
+        "name": "191"
     }
 };
 
-var data = {
+const sampleData = {
     "result": {
         "code": 2020,
         "message": "Movies with the given search parameters found"
@@ -26,59 +26,30 @@ var data = {
 }
 
 const url = new URL(options.baseURL+options.url);
-url.search = new URLSearchParams(options.data).toString();
-
-const headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-};
+url.search = new URLSearchParams(options.data);
 
 let searchButton = document.getElementById("search");   // change to id for search button
 let displayDiv = document.getElementById("mypanel");    // change to div to display
 
 searchButton.onclick = function() {
-    console.log(data);
-    let text = "<table><thead><tr>" +
-        "<th>name</th>" +
-        "<th>sponsor</th>" +
-        "<th>description</th>" +
-        "<th>state</th>" +
-        "<th>course id</th>" +
-        "</tr></thead>" +
-        "<tbody><tr>" +
-        "<td>" + data.project.name + "</td>" +
-        "<td>" + data.project.sponsorName + "</td>" +
-        "<td>" + data.project.description + "</td>" +
-        "<td>" + data.project.state + "</td>" +
-        "<td>" + data.project.courseId + "</td>" +
-        "</tr></tbody></table>";
-    displayDiv.innerHTML = text;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(JSON.stringify(data.project, null, 4));
+            let text = "<table><thead><tr>" +
+                "<th>name</th>" +
+                "<th>sponsor</th>" +
+                "<th>description</th>" +
+                "<th>state</th>" +
+                "<th>course id</th>" +
+                "</tr></thead>" +
+                "<tbody><tr>" +
+                "<td>" + data.project.name + "</td>" +
+                "<td>" + data.project.sponsorName + "</td>" +
+                "<td>" + data.project.description + "</td>" +
+                "<td>" + data.project.state + "</td>" +
+                "<td>" + data.project.courseId + "</td>" +
+                "</tr></tbody></table>";
+            displayDiv.innerHTML = text;
+        })
 };
-
-// searchButton.onclick = function() {
-//     console.log(url);
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(JSON.stringify(data, null, 4));
-//             let text = "<table><thead><tr>" +
-//                 "<th>name</th>" +
-//                 "<th>sponsor</th>" +
-//                 "<th>description</th>" +
-//                 "<th>state</th>" +
-//                 "<th>course id</th>" +
-//                 "</tr></thead>" +
-//                 "<tbody><tr>" +
-//                 "<td>" + data.project.name + "</td>" +
-//                 "<td>" + data.project.sponsorName + "</td>" +
-//                 "<td>" + data.project.description + "</td>" +
-//                 "<td>" + data.project.state + "</td>" +
-//                 "<td>" + data.project.courseId + "</td>" +
-//                 "</tr></tbody></table>";
-//             displayDiv.innerHTML = text;
-//         })
-//         .catch(error => {
-//             alert(error);
-//             console.error(error);
-//         })
-// };
