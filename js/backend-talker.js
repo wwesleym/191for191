fad947041c31315a7a15cd51baf9244241e57ff8
Amen Wondwosen <amenwond408@gmail.com>
2022-05-20 21:12:02 -0700

    hardcoded sample data for testing

--- js/backend-talker.js
+++ js/backend-talker.js
@@ -1,13 +1,32 @@
 const options = {
-    method: "POST",
+    method: "GET",
     baseURL: "http://localhost:8082",
-    url: "/sample/search",  // for endpoints
-    data: { // where to set parameters for each endpoint
-        "name": "191"
+    url: "/projects/search/id",  // for endpoints
+    data: {
+        "id": 1
     }
 };
 
+var data = {
+    "result": {
+        "code": 2020,
+        "message": "Movies with the given search parameters found"
+    },
+    "project": {
+        "id": 1,
+        "name": "191 for 191",
+        "teamSize": 5,
+        "sponsorName": "Matthew Bietz",
+        "description": "A website to share and discover past, present, and future UCI capstones projects.",
+        "video": "https://www.youtube.com/watch?v=UyjVMtTk1XY&list=PLcm9UtazJCOLYIKaXcD1HVB9NGc4kuD0b&index=2",
+        "image": null,
+        "state": "IN_PROGRESS",
+        "courseId": 36050
+    }
+}
+
 const url = new URL(options.baseURL+options.url);
+url.search = new URLSearchParams(options.data).toString();
 
 const headers = {
     "Accept": "application/json",
@@ -18,29 +37,48 @@ let searchButton = document.getElementById("search");   // change to id for sear
 let displayDiv = document.getElementById("mypanel");    // change to div to display
 
 searchButton.onclick = function() {
-    // console.log(headers);
-    fetch(url.toString(), {
-        method: "POST",
-        headers: headers,
-        body: JSON.stringify(options.data)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(JSON.stringify(data.project, null, 4));
-            let text = "<table><thead><tr>" +
-                "<th>name</th>" +
-                "<th>sponsor</th>" +
-                "<th>description</th>" +
-                "<th>state</th>" +
-                "<th>course id</th>" +
-                "</tr></thead>" +
-                "<tbody><tr>" +
-                "<td>" + data.project.name + "</td>" +
-                "<td>" + data.project.sponsorName + "</td>" +
-                "<td>" + data.project.description + "</td>" +
-                "<td>" + data.project.state + "</td>" +
-                "<td>" + data.project.courseId + "</td>" +
-                "</tr></tbody></table>";
-            displayDiv.innerHTML = text;
-        })
+    console.log(data);
+    let text = "<table><thead><tr>" +
+        "<th>name</th>" +
+        "<th>sponsor</th>" +
+        "<th>description</th>" +
+        "<th>state</th>" +
+        "<th>course id</th>" +
+        "</tr></thead>" +
+        "<tbody><tr>" +
+        "<td>" + data.project.name + "</td>" +
+        "<td>" + data.project.sponsorName + "</td>" +
+        "<td>" + data.project.description + "</td>" +
+        "<td>" + data.project.state + "</td>" +
+        "<td>" + data.project.courseId + "</td>" +
+        "</tr></tbody></table>";
+    displayDiv.innerHTML = text;
 };
+
+// searchButton.onclick = function() {
+//     console.log(url);
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(JSON.stringify(data, null, 4));
+//             let text = "<table><thead><tr>" +
+//                 "<th>name</th>" +
+//                 "<th>sponsor</th>" +
+//                 "<th>description</th>" +
+//                 "<th>state</th>" +
+//                 "<th>course id</th>" +
+//                 "</tr></thead>" +
+//                 "<tbody><tr>" +
+//                 "<td>" + data.project.name + "</td>" +
+//                 "<td>" + data.project.sponsorName + "</td>" +
+//                 "<td>" + data.project.description + "</td>" +
+//                 "<td>" + data.project.state + "</td>" +
+//                 "<td>" + data.project.courseId + "</td>" +
+//                 "</tr></tbody></table>";
+//             displayDiv.innerHTML = text;
+//         })
+//         .catch(error => {
+//             alert(error);
+//             console.error(error);
+//         })
+// };
