95ff98ce03ea541e3ccb0a92954544e1b09b3033
Amen Wondwosen <amenwond408@gmail.com>
2022-05-22 15:01:57 -0700

    project data now displayed on projects page
    It does not use the filters to search, but one at least one the filters must be selected for the search button to work.

--- js/backend-talker.js
+++ js/backend-talker.js
@@ -1,5 +1,5 @@
 const options = {
-    method: "POST",
+    method: "GET",
     baseURL: "http://localhost:8082",
     url: "/projects/search/name",
     data: {
@@ -7,49 +7,45 @@ const options = {
     }
 };
 
-const sampleData = {
-    "result": {
-        "code": 2020,
-        "message": "Movies with the given search parameters found"
-    },
-    "project": {
-        "id": 1,
-        "name": "191 for 191",
-        "teamSize": 5,
-        "sponsorName": "Matthew Bietz",
-        "description": "A website to share and discover past, present, and future UCI capstones projects.",
-        "video": "https://www.youtube.com/watch?v=UyjVMtTk1XY&list=PLcm9UtazJCOLYIKaXcD1HVB9NGc4kuD0b&index=2",
-        "image": null,
-        "state": "IN_PROGRESS",
-        "courseId": 36050
-    }
-}
-
 const url = new URL(options.baseURL+options.url);
 url.search = new URLSearchParams(options.data);
 
-let searchButton = document.getElementById("search");   // change to id for search button
-let displayDiv = document.getElementById("mypanel");    // change to div to display
+let searchButton = document.getElementById("submitFilterButton");
+let displayDiv = document.getElementById("project-table-output");
 
 searchButton.onclick = function() {
     fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(JSON.stringify(data.project, null, 4));
-            let text = "<table><thead><tr>" +
-                "<th>name</th>" +
-                "<th>sponsor</th>" +
-                "<th>description</th>" +
-                "<th>state</th>" +
-                "<th>course id</th>" +
-                "</tr></thead>" +
-                "<tbody><tr>" +
-                "<td>" + data.project.name + "</td>" +
-                "<td>" + data.project.sponsorName + "</td>" +
-                "<td>" + data.project.description + "</td>" +
-                "<td>" + data.project.state + "</td>" +
-                "<td>" + data.project.courseId + "</td>" +
-                "</tr></tbody></table>";
-            displayDiv.innerHTML = text;
+            displayDiv.innerHTML = formatResponse(data.project);
         })
 };
+
+function formatResponse(data) {
+    let tableText = "";
+    tableText += `
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Sponsor</th>
+                <th>Description</th>
+                <th>State</th>
+                <th>Course id</th>
+                <th>Video</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>${data.name}</td>
+                <td>${data.sponsorName}</td>
+                <td>${data.description}</td>
+                <td>${data.state}</td>
+                <td>${data.courseId}</td>
+                <td><a href="${data.video}" target='_blank'>link</a></td>
+            </tr>
+        </tbody>
+    </table>`;
+    return tableText;
+}
