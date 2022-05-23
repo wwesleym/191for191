e8cd470eca342205da010ca2ae3d192ae819258e
Amen Wondwosen <amenwond408@gmail.com>
2022-05-22 19:53:31 -0700

    add more functions to grab data
    also fixed issue with functions that get sponsor data

--- backend/src/main/java/inf117/projects/repo/ProjectRepo.java
+++ backend/src/main/java/inf117/projects/repo/ProjectRepo.java
@@ -3,6 +3,7 @@ package inf117.projects.repo;
 import inf117.projects.core.error.ResultError;
 import inf117.projects.core.result.ProjectsResults;
 import inf117.projects.model.request.ProjectSearchRequestModel;
+import inf117.projects.repo.entity.CourseInstance;
 import inf117.projects.repo.entity.Project;
 import inf117.projects.repo.entity.type.CourseTerm;
 import inf117.projects.repo.entity.type.ProjectState;
@@ -45,7 +46,7 @@ public class ProjectRepo {
                     new MapSqlParameterSource()
                             .addValue("name", '%' + name + '%', Types.VARCHAR),
                     (rs, rowNum) -> new Project()
-                            .setId(rs.getInt("p.id"))
+                            .setId(rs.getLong("p.id"))
                             .setName(rs.getString("p.name"))
                             .setTeamSize(rs.getInt("p.team_size"))
                             .setSponsorName(rs.getString("s.name"))
@@ -82,36 +83,43 @@ public class ProjectRepo {
         }
 
         // Insert project in database
-        this.template.update(
-                "INSERT INTO `191for191`.project (id, name, team_size, sponsor_id, " +
-                        "project_description, pitch_video, image, state, course_instance_id) " +
-                        "VALUES (:id, :name, :teamSize, :sponsorId, " +
-                        ":projectDescription, :pitchVideo, :image, :state, :courseId);",
-                new MapSqlParameterSource()
-                        .addValue("id", project.getId(), Types.INTEGER)
-                        .addValue("name", project.getName(), Types.VARCHAR)
-                        .addValue("teamSize", project.getTeamSize(), Types.INTEGER)
-                        .addValue("sponsorId", sponsor.getId(), Types.INTEGER)
-                        .addValue("projectDescription", project.getDescription(), Types.VARCHAR)
-                        .addValue("pitchVideo", project.getVideo(), Types.VARCHAR)
-                        .addValue("image", project.getImage(), Types.VARCHAR)
-                        .addValue("state", project.getState(), Types.VARCHAR)
-                        .addValue("courseId", project.getCourseId(), Types.INTEGER)
-        );
+        try {
+            this.template.update(
+                    "INSERT INTO `191for191`.project (id, name, team_size, sponsor_id, " +
+                            "project_description, pitch_video, image, state, " +
+                            "course_instance_id, year, term) " +
+                            "VALUES (:id, :name, :teamSize, :sponsorId, " +
+                            ":projectDescription, :pitchVideo, :image, :state, :courseId, :year, :term);",
+                    new MapSqlParameterSource()
+                            .addValue("id", project.getId(), Types.INTEGER)
+                            .addValue("name", project.getName(), Types.VARCHAR)
+                            .addValue("teamSize", project.getTeamSize(), Types.INTEGER)
+                            .addValue("sponsorId", sponsor.getId(), Types.INTEGER)
+                            .addValue("projectDescription", project.getDescription(), Types.VARCHAR)
+                            .addValue("pitchVideo", project.getVideo(), Types.VARCHAR)
+                            .addValue("image", project.getImage(), Types.VARCHAR)
+                            .addValue("state", project.getState(), Types.VARCHAR)
+                            .addValue("courseId", project.getCourseId(), Types.INTEGER)
+                            .addValue("year", project.getYear(), Types.INTEGER)
+                            .addValue("term", project.getTerm().toString(), Types.VARCHAR)
+            );
+        } catch (DataAccessException e) {
+            throw new ResultError(ProjectsResults.PROJECT_ALREADY_EXISTS);
+        }
 
     }
 
     public Sponsor selectSponsor(String name) {
         try {
             return this.template.queryForObject(
-                    "SELECT s.id, s.name " +
+                    "SELECT s.id, s.name, s.website " +
                             "FROM `191for191`.sponsor s " +
                             "WHERE s.name LIKE :name ",
                     new MapSqlParameterSource()
                             .addValue("name", '%'+name+'%', Types.VARCHAR),
                     (rs, rowNum) ->
                             new Sponsor()
-                                    .setId(rs.getInt("s.id"))
+                                    .setId(rs.getLong("s.id"))
                                     .setName(rs.getString("s.name"))
                                     .setWebsite(rs.getString("s.website"))
             );
@@ -141,7 +149,7 @@ public class ProjectRepo {
                     new MapSqlParameterSource()
                             .addValue("id", id, Types.INTEGER),
                     (rs, rowNum) -> new Project()
-                            .setId(rs.getInt("p.id"))
+                            .setId(rs.getLong("p.id"))
                             .setName(rs.getString("p.name"))
                             .setTeamSize(rs.getInt("p.team_size"))
                             .setSponsorName(rs.getString("s.name"))
@@ -162,19 +170,51 @@ public class ProjectRepo {
     {
         try {
             return this.template.queryForObject(
-                    "SELECT s.id, s.name " +
+                    "SELECT s.id, s.name, s.website " +
                             "FROM `191for191`.sponsor s " +
                             "WHERE s.id=:id ",
                     new MapSqlParameterSource()
                             .addValue("id", id, Types.INTEGER),
                     (rs, rowNum) ->
                             new Sponsor()
-                                    .setId(rs.getInt("s.id"))
+                                    .setId(rs.getLong("s.id"))
                                     .setName(rs.getString("s.name"))
                                     .setWebsite(rs.getString("s.website"))
             );
         } catch (DataAccessException e) {
             throw new ResultError(ProjectsResults.NO_SPONSOR_WITH_ID_FOUND);
+        }
+    }
+
+    public CourseInstance selectCourseInstance(CourseInstance courseInstance) {
+        try {
+            return this.template.queryForObject(
+                    "SELECT ci.id, ci.year, ci.term, ci.department, ci.number, p.name_first, p.name_first, p.name_middle, p.name_last " +
+                            "FROM `191for191`.course_instance ci " +
+                            "JOIN `191for191`.person p ON p.id = ci.professor_id " +
+                            "WHERE ci.id=:id AND ci.year=:year AND ci.term=:term;",
+                    new MapSqlParameterSource()
+                            .addValue("id", courseInstance.getId(), Types.INTEGER)
+                            .addValue("year", courseInstance.getCourseYear(), Types.INTEGER)
+                            .addValue("term", courseInstance.getCourseTerm().toString(), Types.VARCHAR),
+                    (rs, rowNum) -> {
+                        String professorName = rs.getString("p.name_first") +
+                                " " + rs.getString("p.name_middle") +
+                                " " + rs.getString("p.name_last");
+                        professorName = professorName.replace("\\s\\s+", " ");
+
+                        CourseInstance course = new CourseInstance()
+                                .setId(rs.getInt("ci.id"))
+                                .setDepartment(rs.getString("ci.department"))
+                                .setNumber(rs.getString("ci.number"))
+                                .setCourseYear(rs.getInt("ci.year"))
+                                .setCourseTerm(CourseTerm.fromString(rs.getString("ci.term")));
+
+                        return course.setProfessorName(professorName);
+                    }
+            );
+        } catch (DataAccessException e) {
+            throw new ResultError(ProjectsResults.COURSE_NOT_FOUND);
         }
     }
 }
